49c2fac4976ac40160314397e458e5bd90c53e02
samyakmanandhar124 <2270253@student.bic.edu.np>
2025-07-17 21:28:09 +0545

    Delete mvc1 directory

--- mvc1/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StudentModel model = new StudentModel();
-        StudentView view = new StudentView();
-        new StudentController(model, view);
-    }
-}
